3cfa11a6d68e26d6c8da91f75e5217a52cc8071c
vaishnavihole <vaishnavihole1@gmail.com>
2022-12-09 16:33:24 +0530

    added color ki

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import "./App.css";
 
 function App() {
@@ -10,54 +9,36 @@ function App() {
       <div className="title-container">
         <h1 className="title">Feeling Kit 😊</h1>
       </div>
-      <div className="main-emoji-container">
-        <div className="main-emoji-item">{emoji}</div>
+
+      <div className="emoji-editor-container">
+         <div>
+          <div className="main-emoji-container" style={{backgroundColor: "pink"}}>
+            <div className="main-emoji-item">{emoji}</div>
+          </div>
+          <div className="emoji-container">
+            <div
+              className="emoji" onClick={() => { setEmoji("😍"); }}>😍</div>
+
+            <div className="emoji" onClick={() => { setEmoji("😘"); }} >😘</div>
+
+            <div className="emoji" onClick={() => { setEmoji("😡"); }}>😡</div>
+
+            <div className="emoji" onClick={() => { setEmoji("😭"); }}> 😭 </div>
+
+            <div className="emoji" onClick={() => { setEmoji("😴"); }}> 😴</div>
+          </div>
+        </div>
+
+        <div>
+          <h1>Editor</h1>
+          <div className="colors-container">
+            <div className="color-item bg-red"></div>
+            <div className="color-item bg-black"></div>
+            <div className="color-item bg-orange"></div>
+            <div className="color-item bg-sky"></div>
+            <div className="color-item bg-purple"></div>
+        </div>
       </div>
-      <div className="emoji-container">
-        <div
-          className="emoji"
-          onClick={() => {
-            setEmoji("😍");
-          }}
-        >
-          😍
-        </div>
-
-        <div
-          className="emoji"
-          onClick={() => {
-            setEmoji("😘");
-          }}
-        >
-          😘
-        </div>
-
-        <div
-          className="emoji"
-          onClick={() => {
-            setEmoji("😡");
-          }}
-        >
-          😡
-        </div>
-
-        <div
-          className="emoji"
-          onClick={() => {
-            setEmoji("😭");
-          }}
-        >
-          😭
-        </div>
-
-        <div
-          className="emoji"
-          onClick={() => {
-            setEmoji("😴");
-          }}
-        >
-          😴
-        </div>
       </div>
     </>
   );
